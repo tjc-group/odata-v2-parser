1f77c5ac4943f36acf615e753ede84a515d98232
Sergey Korolev <skorolev@tjc-group.com>
2020-05-16 20:36:42 +0100

    Added resource refernece in error

--- src/error.ts
+++ src/error.ts
@@ -1,8 +1,10 @@
 export class ResourceNotFoundError extends Error {
+    resource: string;
     constructor(resource?: string) {
-        let message = `Resource ${resource} not founc`;
+        let message = `Resource ${resource} not found`;
         super(message);
         this.message = message;
+        this.resource = resource;
         this.name = (this as any).constructor.name;
         Error.captureStackTrace(this, ResourceNotFoundError);
     }
